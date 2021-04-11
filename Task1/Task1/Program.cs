7561efb15f15a95dd2f5ebd605207005697f708e
MOHAMAD AKMAL BIN ZAINODIN <82302428+akmal-95@users.noreply.github.com>
2021-04-11 22:06:07 +0800

    Add files via upload

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Task1
+{
+    class Program
+    {
+		public static void Main()
+		{
+			Console.WriteLine("Task 1");
+			Console.WriteLine("Basic");
+			Console.WriteLine(basic(500000,4.65,30,90).ToString("The Monthly Repayment is RM #.00"));
+		}
+
+		public static double basic(double amount, double rate, double duration, double percent)
+        {
+			double pay = 0;
+			rate = rate / 100;
+			percent = percent / 100;
+			duration = duration * 12;
+			double amountpercentage = amount * percent;
+			pay = ((amountpercentage) * Math.Pow((rate / 12) + 1, (duration)) * rate / 12) / (Math.Pow(rate / 12 + 1, (duration)) - 1);
+			return pay;
+		}
+	}
+}
